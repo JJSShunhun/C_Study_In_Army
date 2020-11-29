6be40d7cc5993bd5b64aaefe3692d92ffb5ab01d
Sanghun Jeong <jeung1129@naver.com>
2020-11-29 14:17:40 +0000

    learn int type and float, double type
    Signed-off-by: Sanghun Jeong <jeung1129@naver.com>

---
+++ Nado_Coding/Printf_Scanf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	//정수형 변수 예제
+	/*int age = 12;
+	printf("%d\n", age);*/
+	
+	//실수형 변수 예제
+	float f = 46.5f;
+	printf("%.2f\n", f);
+	double d = 4.428;
+	printf("%.2lf\n", d);
+	return 0;
+}
