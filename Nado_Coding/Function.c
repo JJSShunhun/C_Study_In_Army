1ce33bf4cf4f5865cacbb3bd13afde3002e24c72
Sanghun Jeong <jeung1129@naver.com>
2020-12-20 13:21:20 +0000

    Learn function 3
    Signed-off-by: Sanghun Jeong <jeung1129@naver.com>

--- Nado_Coding/Function.c
+++ Nado_Coding/Function.c
@@ -10,6 +10,11 @@ void function_with_params(int num1, int num2, int num3);
 
 int apple(int total, int ate); // 전체 total 개에서 ate 개를 먹고 남은 개수를 반환
 
+int add(int num, int num2);
+int sub(int num, int num2);
+int mul(int num, int num2);
+int div(int num, int num2);
+
 int main(void)
 {
 	// function
@@ -56,7 +61,21 @@ int main(void)
 	// 파라미터(전달값)도 받고, 반환값이 있는 함수
 	//int ret = apple(5,2); // 5개의 사과 중에서 2개를 먹었어염
 	//printf("사과 5개 중에 2개를 먹으면 %d 개가 남아요\n", ret);
-	printf("사과 %d 개 중에 %d 개를 먹으면 %d 개가 남아요\n", 10, 4, apple(10,4));
+	//printf("사과 %d 개 중에 %d 개를 먹으면 %d 개가 남아요\n", 10, 4, apple(10,4));
+	
+	//계산기 함수
+	int num = 2;
+	num = add(num, 3);
+	p(num);
+	
+	num = sub(num, 1);
+	p(num);
+	
+	num = mul(num, 3);
+	p(num);
+	
+	num = div(num, 6);
+	p(num);
 	
 	return 0;
 }
@@ -101,3 +120,24 @@ int apple(int total, int ate)
 	printf("전달값과 반환값이 있는 함수입니다.\n");
 	return total - ate;
 }
+
+int add(int num1, int num2)
+{
+	return num1 + num2;
+}
+
+int sub(int num1, int num2)
+{
+	return num1 - num2;
+}
+
+int mul(int num1, int num2)
+{
+	return num1 * num2;
+}
+
+int div(int num1, int num2)
+{
+	return num1 / num2;
+}
+
