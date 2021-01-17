742f1248974aedf450a8c93cf4198a6cd5851e40
Sanghun Jeong <jeung1129@naver.com>
2021-01-17 13:48:47 +0000

    Project : Raising Fish
    Signed-off-by: Sanghun Jeong <jeung1129@naver.com>

--- Nado_Coding/Raising_Fish.c
+++ Nado_Coding/Raising_Fish.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
 // 물고기가 6마리가 있어요
 // 이들은 어항에 살고 있는데, 사막이에요
@@ -13,6 +14,8 @@ int * cursor;
 
 void initData();
 void printfFishes();
+void decreaseWater(long elapsedTime);
+int checkFishAlive();
 
 int main(void)
 {
@@ -46,6 +49,52 @@ int main(void)
 		// 직전 물 준 시간 (마지막으로 물 준 시간) 이후로 흐른 시간
 		prevTime = totalTime - prevTime;
 		printf("최근 경과 시간 : %ld 초\n", prevTime);
+		
+		// 어항의 물을 감소 (증발)
+		decreaseWater(prevTime);
+		
+		// 사용자가 입력한 어항에 물을 준다
+		// 1. 어항의 물이 0 이면? 물을 주지 않는다
+		if(cursor[num - 1] <= 0)
+		{
+			printf("%d 번 물고기는 이미 죽었습니다.. 물을 주지 않습니다\n", num);
+		}
+		// 2. 어항의 물이 0 이 아닌 경우? 물을 준다, 하지만 100을 넘지 않는지 체크
+		else if (cursor[num - 1] + 1 <= 100)
+		{
+			// 물을 준다
+			printf("%d 번 어항에 물을 줍니다\n\n", num);
+			cursor[num - 1] += 1;
+		}
+		
+		// 레벨업을 할 건지 확인 (레벨업은 20초마다 한번씩 수행)
+		if (totalTime / 20 > level - 1)
+		{
+			level++; // level : 1 -> level : 2
+			printf("레벨업! 기존 %d 레벨에서 %d 레벨로 업그레이드", level - 1, level);
+			
+			// 최종 레벨 : 5
+			if (level == 5)
+			{
+				printf("\n\n 축하합니다. 최고 레벨을 달성했습니다. 게임을 종료합니다.\n\n");
+				exit(0);
+			}
+		}
+		
+		// 모든 물고기가 죽었는지 확인
+		if (checkFishAlive() == 0)
+		{
+			// 물고기가 모두 죽음
+			printf("모든 물고기가 죽었습니다...\n");
+		}
+		else
+		{
+			// 최소 한마리 이상의 물고기는 살아 있음
+			printf("물고기가 아직 살아 있어요\n");
+		}
+		prevTime = totalTime;
+		
+		// 최초 물 준 시간 : 10초 -> 15초 (5초 : prevTime -> 15초 로 바꿔!) -> 25초 (10초)
 	}
 	
 	return 0;
@@ -67,4 +116,28 @@ void printfFishes()
 	{
 		printf(" %4d ", arrayFish[i]);
 	}
+}
+
+void decreaseWater(long elapsedTime)
+{
+	for (int i = 0; i < 6; i++)
+	{
+		arrayFish[i] -= (level * 3 * (int)elapsedTime); // 3 : 난이도 조절을 위한 값
+		if (arrayFish[i] < 0)
+		{
+			arrayFish[i] = 0;
+		}
+	}
+}
+
+int checkFishAlive()
+{
+	for (int i = 0; i < 6; i++)
+	{
+		if (arrayFish[i] > 0)
+		{
+			return 1; // True 값
+		}
+	}
+	return 0;
 }
